1efbbd597041f9314fcb3bbbec05a3de33d45a50
anooptoffy <anoop.toffy90@gmail.com>
2018-07-17 18:05:12 +0900

    wavegan

--- development/Supervised_wavegan/train_slwavegan.py
+++ development/Supervised_wavegan/train_slwavegan.py
@@ -195,6 +195,309 @@ def train(fps, args):
       sess.run(G_train_op)
 
 
+"""
+  Creates and saves a MetaGraphDef for simple inference
+  Tensors:
+    'samp_z_n' int32 []: Sample this many latent vectors
+    'samp_z' float32 [samp_z_n, 100]: Resultant latent vectors
+    'z:0' float32 [None, 100]: Input latent vectors
+    'flat_pad:0' int32 []: Number of padding samples to use when flattening batch to a single audio file
+    'G_z:0' float32 [None, 16384, 1]: Generated outputs
+    'G_z_int16:0' int16 [None, 16384, 1]: Same as above but quantizied to 16-bit PCM samples
+    'G_z_flat:0' float32 [None, 1]: Outputs flattened into single audio file
+    'G_z_flat_int16:0' int16 [None, 1]: Same as above but quantized to 16-bit PCM samples
+  Example usage:
+    import tensorflow as tf
+    tf.reset_default_graph()
+
+    saver = tf.train.import_meta_graph('infer.meta')
+    graph = tf.get_default_graph()
+    sess = tf.InteractiveSession()
+    saver.restore(sess, 'model.ckpt-10000')
+
+    z_n = graph.get_tensor_by_name('samp_z_n:0')
+    _z = sess.run(graph.get_tensor_by_name('samp_z:0'), {z_n: 10})
+
+    z = graph.get_tensor_by_name('G_z:0')
+    _G_z = sess.run(graph.get_tensor_by_name('G_z:0'), {z: _z})
+"""
+def infer(args):
+  infer_dir = os.path.join(args.train_dir, 'infer')
+  if not os.path.isdir(infer_dir):
+    os.makedirs(infer_dir)
+
+  # Subgraph that generates latent vectors
+  samp_z_n = tf.placeholder(tf.int32, [], name='samp_z_n')
+  samp_z = tf.random_uniform([samp_z_n, _D_Z], -1.0, 1.0, dtype=tf.float32, name='samp_z')
+
+  # Input zo
+  z = tf.placeholder(tf.float32, [None, _D_Z], name='z')
+  flat_pad = tf.placeholder(tf.int32, [], name='flat_pad')
+
+  # Execute generator
+  with tf.variable_scope('G'):
+    G_z = WaveGANGenerator(z, train=False, **args.wavegan_g_kwargs)
+    if args.wavegan_genr_pp:
+      with tf.variable_scope('pp_filt'):
+        G_z = tf.layers.conv1d(G_z, 1, args.wavegan_genr_pp_len, use_bias=False, padding='same')
+  G_z = tf.identity(G_z, name='G_z')
+
+  # Flatten batch
+  nch = int(G_z.get_shape()[-1])
+  G_z_padded = tf.pad(G_z, [[0, 0], [0, flat_pad], [0, 0]])
+  G_z_flat = tf.reshape(G_z_padded, [-1, nch], name='G_z_flat')
+
+  # Encode to int16
+  def float_to_int16(x, name=None):
+    x_int16 = x * 32767.
+    x_int16 = tf.clip_by_value(x_int16, -32767., 32767.)
+    x_int16 = tf.cast(x_int16, tf.int16, name=name)
+    return x_int16
+  G_z_int16 = float_to_int16(G_z, name='G_z_int16')
+  G_z_flat_int16 = float_to_int16(G_z_flat, name='G_z_flat_int16')
+
+  # Create saver
+  G_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='G')
+  global_step = tf.train.get_or_create_global_step()
+  saver = tf.train.Saver(G_vars + [global_step])
+
+  # Export graph
+  tf.train.write_graph(tf.get_default_graph(), infer_dir, 'infer.pbtxt')
+
+  # Export MetaGraph
+  infer_metagraph_fp = os.path.join(infer_dir, 'infer.meta')
+  tf.train.export_meta_graph(
+      filename=infer_metagraph_fp,
+      clear_devices=True,
+      saver_def=saver.as_saver_def())
+
+  # Reset graph (in case training afterwards)
+  tf.reset_default_graph()
+
+
+"""
+  Generates a preview audio file every time a checkpoint is saved
+"""
+def preview(args):
+  import matplotlib
+  matplotlib.use('Agg')
+  import matplotlib.pyplot as plt
+  from scipy.io.wavfile import write as wavwrite
+  from scipy.signal import freqz
+
+  preview_dir = os.path.join(args.train_dir, 'preview')
+  if not os.path.isdir(preview_dir):
+    os.makedirs(preview_dir)
+
+  # Load graph
+  infer_metagraph_fp = os.path.join(args.train_dir, 'infer', 'infer.meta')
+  graph = tf.get_default_graph()
+  saver = tf.train.import_meta_graph(infer_metagraph_fp)
+
+  # Generate or restore z_i and z_o
+  z_fp = os.path.join(preview_dir, 'z.pkl')
+  if os.path.exists(z_fp):
+    with open(z_fp, 'rb') as f:
+      _zs = pickle.load(f)
+  else:
+    # Sample z
+    samp_feeds = {}
+    samp_feeds[graph.get_tensor_by_name('samp_z_n:0')] = args.preview_n
+    samp_fetches = {}
+    samp_fetches['zs'] = graph.get_tensor_by_name('samp_z:0')
+    with tf.Session() as sess:
+      _samp_fetches = sess.run(samp_fetches, samp_feeds)
+    _zs = _samp_fetches['zs']
+
+    # Save z
+    with open(z_fp, 'wb') as f:
+      pickle.dump(_zs, f)
+
+  # Set up graph for generating preview images
+  feeds = {}
+  feeds[graph.get_tensor_by_name('z:0')] = _zs
+  feeds[graph.get_tensor_by_name('flat_pad:0')] = _WINDOW_LEN // 2
+  fetches = {}
+  fetches['step'] = tf.train.get_or_create_global_step()
+  fetches['G_z'] = graph.get_tensor_by_name('G_z:0')
+  fetches['G_z_flat_int16'] = graph.get_tensor_by_name('G_z_flat_int16:0')
+  if args.wavegan_genr_pp:
+    fetches['pp_filter'] = graph.get_tensor_by_name('G/pp_filt/conv1d/kernel:0')[:, 0, 0]
+
+  # Summarize
+  G_z = graph.get_tensor_by_name('G_z_flat:0')
+  summaries = [
+      tf.summary.audio('preview', tf.expand_dims(G_z, axis=0), _FS, max_outputs=1)
+  ]
+  fetches['summaries'] = tf.summary.merge(summaries)
+  summary_writer = tf.summary.FileWriter(preview_dir)
+
+  # PP Summarize
+  if args.wavegan_genr_pp:
+    pp_fp = tf.placeholder(tf.string, [])
+    pp_bin = tf.read_file(pp_fp)
+    pp_png = tf.image.decode_png(pp_bin)
+    pp_summary = tf.summary.image('pp_filt', tf.expand_dims(pp_png, axis=0))
+
+  # Loop, waiting for checkpoints
+  ckpt_fp = None
+  while True:
+    latest_ckpt_fp = tf.train.latest_checkpoint(args.train_dir)
+    if latest_ckpt_fp != ckpt_fp:
+      print('Preview: {}'.format(latest_ckpt_fp))
+
+      with tf.Session() as sess:
+        saver.restore(sess, latest_ckpt_fp)
+
+        _fetches = sess.run(fetches, feeds)
+
+        _step = _fetches['step']
+
+      preview_fp = os.path.join(preview_dir, '{}.wav'.format(str(_step).zfill(8)))
+      wavwrite(preview_fp, _FS, _fetches['G_z_flat_int16'])
+
+      summary_writer.add_summary(_fetches['summaries'], _step)
+
+      if args.wavegan_genr_pp:
+        w, h = freqz(_fetches['pp_filter'])
+
+        fig = plt.figure()
+        plt.title('Digital filter frequncy response')
+        ax1 = fig.add_subplot(111)
+
+        plt.plot(w, 20 * np.log10(abs(h)), 'b')
+        plt.ylabel('Amplitude [dB]', color='b')
+        plt.xlabel('Frequency [rad/sample]')
+
+        ax2 = ax1.twinx()
+        angles = np.unwrap(np.angle(h))
+        plt.plot(w, angles, 'g')
+        plt.ylabel('Angle (radians)', color='g')
+        plt.grid()
+        plt.axis('tight')
+
+        _pp_fp = os.path.join(preview_dir, '{}_ppfilt.png'.format(str(_step).zfill(8)))
+        plt.savefig(_pp_fp)
+
+        with tf.Session() as sess:
+          _summary = sess.run(pp_summary, {pp_fp: _pp_fp})
+          summary_writer.add_summary(_summary, _step)
+
+      print('Done')
+
+      ckpt_fp = latest_ckpt_fp
+
+    time.sleep(1)
+
+
+"""
+  Computes inception score every time a checkpoint is saved
+"""
+def incept(args):
+  incept_dir = os.path.join(args.train_dir, 'incept')
+  if not os.path.isdir(incept_dir):
+    os.makedirs(incept_dir)
+
+  # Load GAN graph
+  gan_graph = tf.Graph()
+  with gan_graph.as_default():
+    infer_metagraph_fp = os.path.join(args.train_dir, 'infer', 'infer.meta')
+    gan_saver = tf.train.import_meta_graph(infer_metagraph_fp)
+    score_saver = tf.train.Saver(max_to_keep=1)
+  gan_z = gan_graph.get_tensor_by_name('z:0')
+  gan_G_z = gan_graph.get_tensor_by_name('G_z:0')[:, :, 0]
+  gan_step = gan_graph.get_tensor_by_name('global_step:0')
+
+  # Load or generate latents
+  z_fp = os.path.join(incept_dir, 'z.pkl')
+  if os.path.exists(z_fp):
+    with open(z_fp, 'rb') as f:
+      _zs = pickle.load(f)
+  else:
+    gan_samp_z_n = gan_graph.get_tensor_by_name('samp_z_n:0')
+    gan_samp_z = gan_graph.get_tensor_by_name('samp_z:0')
+    with tf.Session(graph=gan_graph) as sess:
+      _zs = sess.run(gan_samp_z, {gan_samp_z_n: args.incept_n})
+    with open(z_fp, 'wb') as f:
+      pickle.dump(_zs, f)
+
+  # Load classifier graph
+  incept_graph = tf.Graph()
+  with incept_graph.as_default():
+    incept_saver = tf.train.import_meta_graph(args.incept_metagraph_fp)
+  incept_x = incept_graph.get_tensor_by_name('x:0')
+  incept_preds = incept_graph.get_tensor_by_name('scores:0')
+  incept_sess = tf.Session(graph=incept_graph)
+  incept_saver.restore(incept_sess, args.incept_ckpt_fp)
+
+  # Create summaries
+  summary_graph = tf.Graph()
+  with summary_graph.as_default():
+    incept_mean = tf.placeholder(tf.float32, [])
+    incept_std = tf.placeholder(tf.float32, [])
+    summaries = [
+        tf.summary.scalar('incept_mean', incept_mean),
+        tf.summary.scalar('incept_std', incept_std)
+    ]
+    summaries = tf.summary.merge(summaries)
+  summary_writer = tf.summary.FileWriter(incept_dir)
+
+  # Loop, waiting for checkpoints
+  ckpt_fp = None
+  _best_score = 0.
+  while True:
+    latest_ckpt_fp = tf.train.latest_checkpoint(args.train_dir)
+    if latest_ckpt_fp != ckpt_fp:
+      print('Incept: {}'.format(latest_ckpt_fp))
+
+      sess = tf.Session(graph=gan_graph)
+
+      gan_saver.restore(sess, latest_ckpt_fp)
+
+      _step = sess.run(gan_step)
+
+      _G_zs = []
+      for i in xrange(0, args.incept_n, 100):
+        _G_zs.append(sess.run(gan_G_z, {gan_z: _zs[i:i+100]}))
+      _G_zs = np.concatenate(_G_zs, axis=0)
+
+      _preds = []
+      for i in xrange(0, args.incept_n, 100):
+        _preds.append(incept_sess.run(incept_preds, {incept_x: _G_zs[i:i+100]}))
+      _preds = np.concatenate(_preds, axis=0)
+
+      # Split into k groups
+      _incept_scores = []
+      split_size = args.incept_n // args.incept_k
+      for i in xrange(args.incept_k):
+        _split = _preds[i * split_size:(i + 1) * split_size]
+        _kl = _split * (np.log(_split) - np.log(np.expand_dims(np.mean(_split, 0), 0)))
+        _kl = np.mean(np.sum(_kl, 1))
+        _incept_scores.append(np.exp(_kl))
+
+      _incept_mean, _incept_std = np.mean(_incept_scores), np.std(_incept_scores)
+
+      # Summarize
+      with tf.Session(graph=summary_graph) as summary_sess:
+        _summaries = summary_sess.run(summaries, {incept_mean: _incept_mean, incept_std: _incept_std})
+      summary_writer.add_summary(_summaries, _step)
+
+      # Save
+      if _incept_mean > _best_score:
+        score_saver.save(sess, os.path.join(incept_dir, 'best_score'), _step)
+        _best_score = _incept_mean
+
+      sess.close()
+
+      print('Done')
+
+      ckpt_fp = latest_ckpt_fp
+
+    time.sleep(1)
+
+  incept_sess.close()
+
+
 if __name__ == '__main__':
   import argparse
   import glob
