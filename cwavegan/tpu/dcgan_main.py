5decfd9f0e2a8d780f2c1a4be2c6039b2763af14
acheketa <cylee@hafs.hs.kr>
2018-07-23 00:02:43 +0900

    tpu

--- cwavegan/tpu/dcgan_main.py
+++ cwavegan/tpu/dcgan_main.py
@@ -94,13 +94,9 @@ def model_fn(features, labels, mode, params):
   random_noise = features['random_noise']
 
   # concatenate
-  #label_fill = tf.expand_dims(labels, axis=2)
-  print(labels)
-  print(random_noise)
-  print(real_images)
-  assert False
+  label_fill = label.reshape([batch_size, 1, 1, 10])
   random_noise = tf.concat([random_noise, labels], 1)
-  real_images = tf.concat([real_images, labels], 1)
+  real_images = tf.concat([real_images, label_fill], 1)
 
   is_training = (mode == tf.estimator.ModeKeys.TRAIN)
   generated_images = model.generator(random_noise,
