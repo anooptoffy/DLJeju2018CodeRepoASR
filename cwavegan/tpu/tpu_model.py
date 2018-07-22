740acc69ec1468c59c273e6b15bd690bdbfe6b18
acheketa <cylee@hafs.hs.kr>
2018-07-22 20:12:02 +0900

    tpu

--- cwavegan/tpu/tpu_model.py
+++ cwavegan/tpu/tpu_model.py
@@ -149,7 +149,7 @@ def discriminator(
   else:
     phaseshuffle = lambda x: x
 
-  with tf.variable_scope('discriminator_0'):
+  with tf.variable_scope('discriminator_0', reuse=tf.AUTO_REUSE):
     # Layer 0
     # [16384, 1] -> [4096, 64]
     output = x
