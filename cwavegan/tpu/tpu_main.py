039c19cd026292030c088f435bf46ba98ee3cf6d
acheketa <cylee@hafs.hs.kr>
2018-07-23 01:58:44 +0900

    tpu

--- cwavegan/tpu/tpu_main.py
+++ cwavegan/tpu/tpu_main.py
@@ -248,8 +248,8 @@ def main(argv):
   while current_step < FLAGS.train_steps:
     next_checkpoint = min(current_step + FLAGS.train_steps_per_eval,
                           FLAGS.train_steps)
-    #est.train(input_fn=generate_input_fn(True),
-    #         max_steps=next_checkpoint)
+    est.train(input_fn=generate_input_fn(True),
+             max_steps=next_checkpoint)
     current_step = next_checkpoint
     tf.logging.info('Finished training step %d' % current_step)
 
