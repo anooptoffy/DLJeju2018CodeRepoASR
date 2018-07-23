a9637219f082df8c3ed33e6adb0b1ef920f9ba62
acheketa <cylee@hafs.hs.kr>
2018-07-23 21:11:59 +0900

    tpu

--- cwavegan/tpu/tpu1_input.py
+++ cwavegan/tpu/tpu1_input.py
@@ -89,6 +89,8 @@ class InputFunction(object):
 
     random_noise = tf.random_uniform([batch_size, self.noise_dim], -1., 1., dtype=tf.float32)
     print(labels)
+    labels = labels + tf.constant(10, name='fixed')
+    print("here",labels)
     assert False
     features = {
         'real_audio': wav,
